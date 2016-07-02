47ff04b9adfbf733e9caef0f5a8b68012df67a54
miconst <0.miconst@gmail.com>
2016-07-02 10:21:29 +0300

    + MasterP solution

---
+++ MasterP/MasterP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace MasterP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("word #1: ");
+            var w1 = Console.ReadLine();
+
+            Console.Write("word #2: ");
+            var w2 = Console.ReadLine();
+
+            Console.Write("from: ");
+            int from = Int32.Parse(Console.ReadLine());
+
+            Console.Write("to: ");
+            int to = Int32.Parse(Console.ReadLine());
+
+            using (var md5 = MD5.Create())
+            {
+                byte[] bytes = Encoding.ASCII.GetBytes(w1 + '-' + w2 + '\n');
+                byte[] hash_code = md5.ComputeHash(bytes);
+                byte[] hex_code = new byte[hash_code.Length * 2];
+
+                for(int i = 0; i < hash_code.Length; i++)
+                {
+                    byte b = hash_code[i];
+                    var hb = (b >> 4);
+                    var lb = (b & 0xF);
+
+                    hex_code[i * 2    ] = (byte)(hb < 10 ? '0' + hb : 'a' + hb - 10);
+                    hex_code[i * 2 + 1] = (byte)(lb < 10 ? '0' + lb : 'a' + lb - 10);
+                }
+
+                //string x = BitConverter.ToString(hash_code).ToLower().Replace("-", "");
+                //byte[] x_bytes = System.Text.Encoding.UTF8.GetBytes(x);
+                var s = Convert.ToBase64String(hex_code);
+
+                Console.WriteLine(s.Substring(from - 1, to - from + 1));
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
