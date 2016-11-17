68cf1bb2116eada3fbd8e2f317e9fdc3f077f990
0-micon <0.miconst@gmail.com>
2016-11-17 18:06:58 +0300

    Designer Update

--- MasterP/MasterP/MainForm.Designer.cs
+++ MasterP/MasterP/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace MasterP
+namespace MasterP
 {
     partial class MainForm
     {
@@ -54,65 +54,70 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.Size = new System.Drawing.Size(52, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "word #1";
+            this.label1.Text = "Word #1:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Location = new System.Drawing.Point(12, 36);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "word #2";
+            this.label2.Text = "Word #2:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(71, 39);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(71, 9);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.UseSystemPasswordChar = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(71, 66);
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(71, 36);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(173, 20);
             this.textBox2.TabIndex = 3;
             this.textBox2.UseSystemPasswordChar = true;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 95);
+            this.label3.Location = new System.Drawing.Point(12, 65);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.Size = new System.Drawing.Size(33, 13);
             this.label3.TabIndex = 4;
-            this.label3.Text = "from";
+            this.label3.Text = "From:";
             // 
             // label4
             // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(18, 126);
+            this.label4.Location = new System.Drawing.Point(155, 65);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.Size = new System.Drawing.Size(23, 13);
             this.label4.TabIndex = 5;
-            this.label4.Text = "to";
+            this.label4.Text = "To:";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(71, 95);
+            this.numericUpDown1.Location = new System.Drawing.Point(71, 62);
             this.numericUpDown1.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(61, 20);
             this.numericUpDown1.TabIndex = 8;
             this.numericUpDown1.Value = new decimal(new int[] {
             1,
@@ -122,14 +127,15 @@
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(71, 126);
+            this.numericUpDown2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown2.Location = new System.Drawing.Point(183, 62);
             this.numericUpDown2.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown2.Size = new System.Drawing.Size(61, 20);
             this.numericUpDown2.TabIndex = 9;
             this.numericUpDown2.Value = new decimal(new int[] {
             1,
@@ -139,7 +145,7 @@
             // 
             // buttonClipboard
             // 
-            this.buttonClipboard.Location = new System.Drawing.Point(15, 226);
+            this.buttonClipboard.Location = new System.Drawing.Point(12, 117);
             this.buttonClipboard.Name = "buttonClipboard";
             this.buttonClipboard.Size = new System.Drawing.Size(75, 23);
             this.buttonClipboard.TabIndex = 10;
@@ -150,16 +156,18 @@
             // 
             // textBoxResult
             // 
-            this.textBoxResult.Location = new System.Drawing.Point(71, 177);
+            this.textBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxResult.Location = new System.Drawing.Point(71, 91);
             this.textBoxResult.Name = "textBoxResult";
             this.textBoxResult.ReadOnly = true;
-            this.textBoxResult.Size = new System.Drawing.Size(126, 20);
+            this.textBoxResult.Size = new System.Drawing.Size(173, 20);
             this.textBoxResult.TabIndex = 11;
             this.textBoxResult.UseSystemPasswordChar = true;
             // 
             // buttonReset
             // 
-            this.buttonReset.Location = new System.Drawing.Point(122, 226);
+            this.buttonReset.Location = new System.Drawing.Point(96, 117);
             this.buttonReset.Name = "buttonReset";
             this.buttonReset.Size = new System.Drawing.Size(75, 23);
             this.buttonReset.TabIndex = 12;
@@ -171,20 +179,21 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 180);
+            this.label5.Location = new System.Drawing.Point(12, 94);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.Size = new System.Drawing.Size(40, 13);
             this.label5.TabIndex = 13;
-            this.label5.Text = "r&esult";
+            this.label5.Text = "R&esult:";
             // 
             // checkBox1
             // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
             this.checkBox1.AutoSize = true;
             this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.checkBox1.ImageIndex = 0;
             this.checkBox1.ImageList = this.imageList1;
-            this.checkBox1.Location = new System.Drawing.Point(173, 38);
+            this.checkBox1.Location = new System.Drawing.Point(250, 7);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(22, 22);
             this.checkBox1.TabIndex = 14;
@@ -201,12 +210,13 @@
             // 
             // checkBox2
             // 
+            this.checkBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.checkBox2.Appearance = System.Windows.Forms.Appearance.Button;
             this.checkBox2.AutoSize = true;
             this.checkBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.checkBox2.ImageIndex = 0;
             this.checkBox2.ImageList = this.imageList1;
-            this.checkBox2.Location = new System.Drawing.Point(173, 65);
+            this.checkBox2.Location = new System.Drawing.Point(250, 34);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(22, 22);
             this.checkBox2.TabIndex = 15;
@@ -216,12 +226,13 @@
             // 
             // checkBoxResult
             // 
+            this.checkBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.checkBoxResult.Appearance = System.Windows.Forms.Appearance.Button;
             this.checkBoxResult.AutoSize = true;
             this.checkBoxResult.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.checkBoxResult.ImageIndex = 0;
             this.checkBoxResult.ImageList = this.imageList1;
-            this.checkBoxResult.Location = new System.Drawing.Point(199, 176);
+            this.checkBoxResult.Location = new System.Drawing.Point(250, 89);
             this.checkBoxResult.Name = "checkBoxResult";
             this.checkBoxResult.Size = new System.Drawing.Size(22, 22);
             this.checkBoxResult.TabIndex = 16;
@@ -237,7 +248,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 144);
             this.Controls.Add(this.checkBoxResult);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
@@ -283,4 +294,3 @@
         private System.Windows.Forms.ToolTip toolTip1;
     }
 }
-
