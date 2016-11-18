a0ef337082be47e114ed91f9a54d6937563f600b
mico <mi-const@yandex.ru>
2016-11-18 16:45:39 +0300

    + Direction class

---
+++ MagicCube/MagicCube/Direction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCube
+{
+    enum Turn { Up, Right, Down, Left };
+
+    class Direction
+    {
+        public const int
+            UP = 0,
+            RIGHT = 1,
+            DOWN = 2,
+            LEFT = 3,
+            TURN_COUNT = 4;
+
+        // relative rotation
+        public static int Sum(int a, int b)
+        {
+            return (a + b) % TURN_COUNT;
+        }
+
+        // 90 degree clockwise rotation (right-hand rule)
+        public static int TurnRight(int direction)
+        {
+            return Sum(direction, RIGHT);
+        }
+
+        // 90 degree anticlockwise rotation
+        public static int TurnLeft(int direction)
+        {
+            return Sum(direction, LEFT);
+        }
+
+        // 180 degree turn
+        public static int TurnAround(int direction)
+        {
+            return Sum(direction, DOWN);
+        }
+    }
+}
