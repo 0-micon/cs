d6d92daffb7c5ef1e1b0314aa6b7a6e73cb4d607
mico <mi-const@yandex.ru>
2017-01-25 12:14:52 +0300

    + General solution

---
+++ MagicCube/MagicCube/CubeGeneralSolution.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCube
+{
+    class CubeGeneralSolution<K, T> : GeneralSolution<K> where T : Faces.IRotatable
+    {
+        public MoveTrack PathTo(K key, Func<K, T> KtoT, Func<T, K> TtoK)
+        {
+            MoveTrack path = new MoveTrack();
+            for (int i = Count; i-- > 0;)
+            {
+                foreach (var pair in Faces.NextMoves(KtoT(key)))
+                {
+                    K dst_key = TtoK(pair.Value);
+                    if (this[i].BinarySearch(dst_key) >= 0)
+                    {
+                        key = dst_key;
+                        path.Add(pair.Key);
+                        break;
+                    }
+                }
+            }
+            return path;
+        }
+
+        public MoveTrack SolveCube(K start_key, int depth, Func<K, T> KtoT, Func<T, K> TtoK)
+        {
+            List<K> ring = new List<K>();
+            ring.Add(start_key);
+
+            CubeGeneralSolution<K, T> l_rings = new CubeGeneralSolution<K, T>();
+            l_rings.Add(ring);
+
+            int reserved = 18;
+            while (--depth > 0)
+            {
+                List<K> next_ring = null;
+                if (depth > 0)
+                {
+                    next_ring = new List<K>(reserved);
+                }
+
+                foreach (K key in ring)
+                {
+                    foreach (K next_key in Faces.NextKeys(key, KtoT, TtoK))
+                    {
+                        int pos = this.FindRow(next_key);
+                        if (pos >= 0)
+                        {
+                            MoveTrack path = l_rings.PathTo(next_key, KtoT, TtoK);
+                            path = path.Reverse();
+                            path += PathTo(next_key, KtoT, TtoK);
+                            return path;
+                        }
+                        else if (next_ring != null && l_rings.FindRow(next_key) < 0)
+                        {
+                            next_ring.Add(next_key);
+                        }
+                    }
+                }
+
+                reserved = next_ring.Count * 13;
+                next_ring.DistinctValues();
+                l_rings.Add(next_ring);
+                ring = next_ring;
+            }
+            return null;
+        }
+    }
+}
---
+++ MagicCube/MagicCube/GeneralSolution.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCube
+{
+    // GeneralSolution is a very general problem-solving technique that
+    // consists of systematically enumerating all possible candidates for the
+    // solution and checking whether each candidate satisfies the problem's statement.
+    class GeneralSolution<K> : List<List<K>>
+    {
+        public void PrecomputeMoves(K start_key, int max_depth, int reserved, int growth_rate, Func<K, IEnumerable<K>> next)
+        {
+            List<K> src_ring = new List<K>(1);
+            src_ring.Add(start_key);
+
+            Clear();
+            Capacity = max_depth;
+            Add(src_ring);
+
+            while (Count < max_depth)
+            {
+                List<K> next_ring = new List<K>(reserved);
+                foreach (K src_key in src_ring)
+                {
+                    foreach (K dst_key in next(src_key))
+                    {
+                        if (this.FindRow(dst_key) < 0)
+                        {
+                            next_ring.Add(dst_key);
+                        }
+                    }
+                }
+
+                reserved = next_ring.Count * growth_rate;
+                // sort and remove duplicates
+                next_ring.DistinctValues();
+
+                Add(next_ring);
+                src_ring = next_ring;
+            }
+        }
+
+        public void Save(string fname, string extension, Action<BinaryWriter, K> saver)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                this[i].Save(fname + $"{i}." + extension, saver);
+            }
+        }
+
+        public void Load(string fname, string extension, int capacity, int element_size, Func<BinaryReader, K> reader)
+        {
+            Clear();
+            Capacity = capacity;
+
+            for (int i = 0; i < capacity; i++)
+            {
+                var list = new List<K>();
+                list.Load(fname + $"{i}." + extension, element_size, reader);
+                Add(list);
+            }
+        }
+    }
+}
